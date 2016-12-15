269278def83ed6055fb9b7a9fb6530e77d6b8a40
lvasquez <lvasquez@192.168.0.10>
2016-12-15 16:43:31 -0600

    Servidor.

--- TRON-PARENT/TRON-SERVER-PARENT/tron-server-integration/src/main/java/com/bytesw/trg/tron/server/integration/StartupContext.java
+++ TRON-PARENT/TRON-SERVER-PARENT/tron-server-integration/src/main/java/com/bytesw/trg/tron/server/integration/StartupContext.java
@@ -1,9 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bytesw.trg.tron.server.integration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -11,8 +12,32 @@ package com.bytesw.trg.tron.server.integration;
  */
 public class StartupContext {
 
+        static org.slf4j.Logger logger = LoggerFactory.getLogger(StartupContext.class);
+
         public static void main(String[] args) {
-                
+
+                Properties properties = new Properties();
+                logger.info("Tron Server V 1.0");
+                try {
+                        if (args != null && args.length > 0) {
+                                logger.info("Iniciando carga de propiedades");
+
+                                properties.load(new FileInputStream(new File(args[0])));
+                                for (Object object : properties.keySet()) {
+                                        String key = object.toString();
+                                        String value = properties.getProperty(key);
+                                        logger.info("Propiedad [" + key + "]: [" + value + "] ");
+                                        System.setProperty(key, value);
+                                }
+                        } else {
+                                logger.info("No existen propiedades externas, se utilizarán las defecto.");
+                        }
+                } catch (Exception ex) {
+                        logger.error("Error cargando propiedades externas, se utilizarán las defecto.", ex);
+                }
+
+                //        "classpath:/com/bytesw/ocs/rpi/ri/bs/spring/applicationContext-ocs-ri-persistence.xml"
+                ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/com/bytesw/trg/tron/server/services/bs/spring/applicationContext-services.xml");
         }
-        
+
 }
--- TRON-PARENT/TRON-SERVER-PARENT/tron-server-services/src/main/java/com/bytesw/trg/tron/server/services/bs/TronServiceImpl.java
+++ TRON-PARENT/TRON-SERVER-PARENT/tron-server-services/src/main/java/com/bytesw/trg/tron/server/services/bs/TronServiceImpl.java
@@ -1,11 +1,88 @@
 package com.bytesw.trg.tron.server.services.bs;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author lvasquez
  */
-public class TronServiceImpl 
-//implements TronService
-{
-        
+public class TronServiceImpl implements TronService, ServiceListener {
+
+        static org.slf4j.Logger logger = LoggerFactory.getLogger(TronServiceImpl.class);
+
+        private Integer serverListenPort;
+        private String serviceType;
+        private String serviceName;
+        private String serviceText;
+        JmDNS jmdns;
+
+        public void init() {
+                try {
+                        logger.info("Registrando servidor");
+                        jmdns = JmDNS.create(InetAddress.getLocalHost());
+                        ServiceInfo serviceInfo = ServiceInfo.create(serviceType, serviceName, serverListenPort, serviceText);
+                        jmdns.registerService(serviceInfo);
+                        logger.info("Servidor registrado");
+                } catch (IOException ex) {
+                        logger.error("Error de inicializacion.", ex);
+                }
+        }
+
+        public void destroy() {
+                jmdns.unregisterAllServices();
+        }
+
+        public Integer getServerListenPort() {
+                return serverListenPort;
+        }
+
+        public void setServerListenPort(Integer serverListenPort) {
+                this.serverListenPort = serverListenPort;
+        }
+
+        public String getServiceType() {
+                return serviceType;
+        }
+
+        public void setServiceType(String serviceType) {
+                this.serviceType = serviceType;
+        }
+
+        public String getServiceName() {
+                return serviceName;
+        }
+
+        public void setServiceName(String serviceName) {
+                this.serviceName = serviceName;
+        }
+
+        public String getServiceText() {
+                return serviceText;
+        }
+
+        public void setServiceText(String serviceText) {
+                this.serviceText = serviceText;
+        }
+
+        @Override
+        public void serviceAdded(ServiceEvent se) {
+                logger.info("Service Added [" + se.getInfo() + "]");
+        }
+
+        @Override
+        public void serviceRemoved(ServiceEvent se) {
+                logger.info("Service Removed [" + se.getInfo() + "]");
+        }
+
+        @Override
+        public void serviceResolved(ServiceEvent se) {
+                logger.info("Service Resolved [" + se.getInfo() + "]");
+        }
+
 }
